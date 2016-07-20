a6b61040ed326db0b682293d205faca4e6415fab
Vlad Ursu <ursu.vlad94@yahoo.com>
2016-07-20 14:02:28 +0300

    Added Customer classes: interface and implementation.

---
+++ ITPManager/src/Customer.java
@@ -0,0 +1,142 @@
+import org.joda.time.LocalDate;
+
+public interface Customer {
+
+	/**
+	 * Get the customer's ID.
+	 *
+	 * @return the customer's ID
+	 */
+	public Integer getId();
+
+	/**
+	 * Set the customer's ID.
+	 *
+	 * @param id
+	 *            the ID of the customer.
+	 */
+	public void setId(Integer id);
+
+	/**
+	 * Get the customer's name.
+	 *
+	 * @return the customer's name.
+	 */
+	public String getName();
+
+	/**
+	 * Set the customer's name.
+	 *
+	 * @param name
+	 *            the name of the customer.
+	 */
+	public void setName(String name);
+
+	/**
+	 * Get the customer's car model.
+	 *
+	 * @return the customer's car model.
+	 */
+	public String getCarModel();
+
+	/**
+	 * Set the customer's car model.
+	 *
+	 * @param car_model
+	 *            the car model of the customer.
+	 */
+	public void setCarModel(String car_model);
+
+	/**
+	 * Get the customer's registration id.
+	 *
+	 * @return the customer's registration id.
+	 */
+	public String getRegistId();
+
+	/**
+	 * Set the customer's registration id.
+	 *
+	 * @param regist_id
+	 *            the registration id of the customer.
+	 */
+	public void setRegistId(String regist_id);
+
+	/**
+	 * Get the customer's email.
+	 *
+	 * @return the customer's email.
+	 */
+	public String getEmail();
+
+	/**
+	 * Set the customer's email address.
+	 *
+	 * @param email
+	 *            the email address of the customer.
+	 */
+	public void setEmail(String email);
+
+	/**
+	 * Get the customer's phone number.
+	 *
+	 * @return the customer's phone number.
+	 */
+	public String getPhoneNr();
+
+	/**
+	 * Set the customer's phone number.
+	 *
+	 * @param phone_nr
+	 *            the phone number of the customer.
+	 */
+	public void setPhoneNr(String phone_nr);
+
+	/**
+	 * Get the customer's ITP end date.
+	 *
+	 * @return the customer's ITP end date.
+	 */
+	public LocalDate getITPEndDate();
+
+	/**
+	 * Set the customer's ITP end date.
+	 *
+	 * @param itp_end_date
+	 *            the ITP end date of the customer.
+	 */
+	public void setITPEndDate(LocalDate itp_end_date);
+
+	/**
+	 * Get the state of the customer. Either true if they have been notified via
+	 * email or false otherwise.
+	 *
+	 * @return the status indicator.
+	 */
+	public boolean getEmailSent();
+
+	/**
+	 * Set the customer's state. Either true if they have been notified via
+	 * email or false otherwise.
+	 *
+	 * @param email_sent
+	 *            the status indicator.
+	 */
+	public void setEmailSent(boolean email_sent);
+
+	/**
+	 * Get other comments about this customer
+	 *
+	 * @return comments about customer
+	 */
+	public String getOther();
+
+	/**
+	 * Set the comments for this customer.
+	 *
+	 * @param other
+	 *            the comments about the customer.
+	 */
+	public void setOther(String other);
+
+}
---
+++ ITPManager/src/CustomerImpl.java
@@ -0,0 +1,103 @@
+import org.joda.time.LocalDate;
+
+public class CustomerImpl implements Customer {
+
+	private Integer id;
+	private String name;
+	private String car_model;
+	private String regist_id;
+	private String email;
+	private String phone_nr;
+	private LocalDate itp_end_date;
+	private boolean email_sent;
+	private String other;
+	
+	public CustomerImpl(Integer id, String name, String car_model, String regist_id, String email, String phone_nr,
+			LocalDate itp_end_date, boolean email_sent, String other) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.car_model = car_model;
+		this.regist_id = regist_id;
+		this.email = email;
+		this.phone_nr = phone_nr;
+		this.itp_end_date = itp_end_date;
+		this.email_sent = email_sent;
+		this.other = other;
+	}
+	
+	public CustomerImpl() {}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCarModel() {
+		return car_model;
+	}
+
+	public void setCarModel(String car_model) {
+		this.car_model = car_model;
+	}
+
+	public String getRegistId() {
+		return regist_id;
+	}
+
+	public void setRegistId(String regist_id) {
+		this.regist_id = regist_id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNr() {
+		return phone_nr;
+	}
+
+	public void setPhoneNr(String phone_nr) {
+		this.phone_nr = phone_nr;
+	}
+
+	public LocalDate getITPEndDate() {
+		return itp_end_date;
+	}
+
+	public void setITPEndDate(LocalDate itp_end_date) {
+		this.itp_end_date = itp_end_date;
+	}
+
+	public boolean getEmailSent() {
+		return email_sent;
+	}
+
+	public void setEmailSent(boolean email_sent) {
+		this.email_sent = email_sent;
+	}
+
+	public String getOther() {
+		return other;
+	}
+
+	public void setOther(String other) {
+		this.other = other;
+	};
+	
+}
